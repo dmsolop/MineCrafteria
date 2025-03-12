ba5c0924933c1eb486cc1ad75156f804896a76bb
Dmytro Solop <dmtsolop@gmail.com>
2025-03-12 19:42:39 +0200

    - Integrated **Firebase Remote Config** - first step. - Fixed **R8 minification issues**:   - Added missing **ProGuard rules** to prevent Google Play Core & Facebook Ads classes from being removed.   - Updated `proguard-rules.pro` with required `-keep` rules. - Fixed **Keystore signing issues**:   - Finded missing `melon-keystore.jks` and relocate it.   - Corrected `key.properties` and `build.gradle` to reference the correct path.   - Ensured `signingConfig` is properly configured. - Updated **build.gradle dependencies**:   - Verified correct **Firebase** dependencies. - Performed **clean build** after changes:   - `flutter clean`   - `rm -rf android/.gradle`   - `flutter pub get`   - `flutter build apk --release` (successful 🎉)

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,26 @@
+# Keep all classes of Facebook Ads SDK
+-keep class com.facebook.** { *; }
+-keep class com.facebook.infer.annotation.** { *; }
+-dontwarn com.facebook.**
+
+# Exclude checking of certain annotations
+-keepattributes *Annotation*
+
+# Prevent R8 errors
+-keep class * {
+    @com.facebook.infer.annotation.* *;
+}
+# Keep Google Play Core classes
+-keep class com.google.android.play.** { *; }
+-dontwarn com.google.android.play.**
+
+# Prevent R8 from removing Deferred Components API
+-keep class io.flutter.embedding.engine.deferredcomponents.** { *; }
+
+# Keep SplitInstallManager and related classes
+-keep class com.google.android.play.core.splitinstall.** { *; }
+-keep class com.google.android.play.core.tasks.** { *; }
+
+# Ensure SplitCompatApplication is not removed
+-keep class com.google.android.play.core.splitcompat.SplitCompatApplication { *; }
+
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_core
+import firebase_remote_config
 import flutter_localization
 import in_app_review
 import package_info_plus
@@ -16,6 +18,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
