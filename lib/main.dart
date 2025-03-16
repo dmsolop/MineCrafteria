428025a55501e4e235ffff3846b42c472764c6ac
Dmytro Solop <dmtsolop@gmail.com>
2025-03-16 23:59:29 +0200

    Minor refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'backend/NativeAdManager.dart';
-import 'frontend/NativeAdWidget.dart';
 
 //test gap
 final FlutterLocalization localization = FlutterLocalization.instance;
