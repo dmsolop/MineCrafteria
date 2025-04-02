4de833884bca0bc3616eb98eee771e64297d9ef9
Dmytro Solop <dmtsolop@gmail.com>
2025-04-02 18:45:36 +0300

    Temporarily disabled Minecraft installation check for testing purposes

--- lib/backend/FileOpener.dart
+++ lib/backend/FileOpener.dart
@@ -9,8 +9,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 class FileOpener {
-  static const MethodChannel _channel =
-      MethodChannel('com.morph.mods.minecraft.addons/file_opener');
+  static const MethodChannel _channel = MethodChannel('com.morph.mods.minecraft.addons/file_opener');
 
   static Future<Directory> getTempDirectory() async {
     if (Platform.isIOS) {
@@ -20,35 +19,32 @@ class FileOpener {
     }
   }
 
-  static Future<void> openFileWithApp(String filePath, double screenWidth,
-      double screenHeight, BuildContext context) async {
-    if (!(await AppCheck().isAppInstalled("com.mojang.minecraftpe")) &&
-        Platform.isAndroid) {
-      Navigator.of(context, rootNavigator: true).pop();
-      showDialog<void>(
-        context: context,
-        barrierDismissible: false, // user must tap button!
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: SingleChildScrollView(
-              child: Center(
-                child: Text(
-                  AppLocale.error_app.getString(context),
-                  style: const TextStyle(fontFamily: "Joystix"),
-                ),
-              ),
-            ),
-          );
-        },
-      );
-      await Future.delayed(const Duration(seconds: 3));
-      return;
-    }
+  static Future<void> openFileWithApp(String filePath, double screenWidth, double screenHeight, BuildContext context) async {
+    // if (!(await AppCheck().isAppInstalled("com.mojang.minecraftpe")) &&
+    //     Platform.isAndroid) {
+    //   Navigator.of(context, rootNavigator: true).pop();
+    //   showDialog<void>(
+    //     context: context,
+    //     barrierDismissible: false, // user must tap button!
+    //     builder: (BuildContext context) {
+    //       return AlertDialog(
+    //         content: SingleChildScrollView(
+    //           child: Center(
+    //             child: Text(
+    //               AppLocale.error_app.getString(context),
+    //               style: const TextStyle(fontFamily: "Joystix"),
+    //             ),
+    //           ),
+    //         ),
+    //       );
+    //     },
+    //   );
+    //   await Future.delayed(const Duration(seconds: 3));
+    //   return;
+    // }
 
     if (Platform.isIOS) {
-      await Share.shareXFiles([XFile(filePath)],
-          sharePositionOrigin:
-              Rect.fromLTWH(0, 0, screenWidth, screenHeight / 2));
+      await Share.shareXFiles([XFile(filePath)], sharePositionOrigin: Rect.fromLTWH(0, 0, screenWidth, screenHeight / 2));
     } else {
       try {
         await _channel.invokeMethod('openFileWithApp', {'filePath': filePath});
